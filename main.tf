23c6c7d6727bad4833afbe27a3e07c7021257f92
Anton Samarchyan <desecho@gmail.com>
2022-07-10 18:42:02 -0400

    Changes

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "digitalocean" {}
 resource "digitalocean_kubernetes_cluster" "scrap_k8s_cluster" {
   name          = "scrap"
   region        = "tor1"
-  version       = "1.20.2-do.0"
+  version       = "1.21.11-do.1"
   auto_upgrade  = true
   surge_upgrade = true
 
   node_pool {
     name       = "worker-pool"
     size       = "s-1vcpu-2gb"
-    node_count = 1
+    node_count = 2
   }
 }
